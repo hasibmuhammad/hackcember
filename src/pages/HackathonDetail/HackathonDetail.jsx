e67b83230e34a4e1e6ac1f4fbcefb12ffc712a0b
hasibmuhammad <hasibmu99@gmail.com>
2023-11-22 12:34:15 +0600

    hackathon detail page - done

--- src/pages/HackathonDetail/HackathonDetail.jsx
+++ src/pages/HackathonDetail/HackathonDetail.jsx
@@ -12,8 +12,16 @@ const HackathonDetail = () => {
   const { id } = useParams();
   const hackathons = useLoaderData();
   const hackathon = hackathons.find((ht) => ht.id === parseInt(id));
-  const { name, image, fees, prize_money, long_description, location, date } =
-    hackathon;
+  const {
+    name,
+    image,
+    fees,
+    prize_money,
+    long_description,
+    location,
+    date,
+    badges,
+  } = hackathon;
 
   console.log(date);
 
@@ -31,34 +39,51 @@ const HackathonDetail = () => {
         </div>
       </div>
       <div className="max-w-7xl mx-auto my-4 px-10 lg:px-0">
-        <div className="flex gap-24">
+        <h1 className="text-4xl font-bold">Hackathon Detail</h1>
+        <div className="flex flex-col md:flex-row md:gap-24 mt-10">
           <div>
             <p className="text-lg flex items-center gap-1">
-              <MdEventNote />
-              <span className="font-bold">Event: </span>
+              <span className="text-main">
+                <MdEventNote />
+              </span>
               {name}
             </p>
             <p className="flex items-center gap-1">
-              <FaLocationDot />
+              <span className="text-main">
+                <FaLocationDot />
+              </span>
               {location}
             </p>
             <p className="flex items-center gap-1">
-              <FaCalendarCheck />
+              <span className="text-main">
+                <FaCalendarCheck />
+              </span>
               {date}
             </p>
           </div>
           <div>
             <p className="flex items-center gap-1 text-lg">
-              <FaCcMastercard />
+              <span className="text-main">
+                <FaCcMastercard />
+              </span>
               <span className="font-bold">Fees: </span>${fees}
             </p>
             <p className="flex items-center gap-1 text-lg">
-              <GrAchievement />
+              <span className="text-main">
+                <GrAchievement />
+              </span>
               <span className="font-bold">Prize: </span>${prize_money}
             </p>
           </div>
         </div>
         <p className="mt-4 text-lg text-justify">{long_description}</p>
+        <div className="flex items-center gap-3 flex-wrap mt-4">
+          {badges.map((badge) => (
+            <div className="bg-main px-3 py-.5 text-white font-medium rounded-full text-sm">
+              #{badge}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
